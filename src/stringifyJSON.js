a0579b88e9518640f28e4e8e35204c3724189dab
fmaredia <farhana.maredia@gmail.com>
2015-05-30 18:47:11 -0500

    stringify complete

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,30 @@
 
 // but you don't so you're going to write it from scratch:
 
+// your code goes here
+
 var stringifyJSON = function(obj) {
-  // your code goes here
+  var result = [];
+
+  if (obj == null) {
+    return "null";
+  } else if (obj === undefined) {
+    return undefined;
+  } else if (typeof obj === "string") {
+    return '"' + obj.toString() + '"';
+  } else if (Array.isArray(obj)) {
+    for (var i = 0; i < obj.length; i++) {
+      result.push(stringifyJSON(obj[i])); 
+    }
+    return "[" + result + "]";
+  } else if (typeof obj === "object") {
+      for (var key in obj) {
+        if (obj.hasOwnProperty(key) && !(key == "functions") && !(key == "undefined")) {
+          result.push(stringifyJSON(key) + ":" + stringifyJSON(obj[key]));
+      }
+    }
+    return "{" + result.toString() + "}";
+  } else {
+    return obj.toString();
+  }
 };
